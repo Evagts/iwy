022106504f7aa3d69f53824f075acca1d569be90
Alexander Robles <alexanderrobles12@outlook.com>
2024-06-24 19:56:08 -0600

    Updates

---
+++ precache-manifest.d509c29686bc2169299f24ccaf67a43c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8a76071a604bdf134b4b941daf05ba",
+    "url": "/iwy/index.html"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/iwy/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "7d6b9f8978281162c50d",
+    "url": "/iwy/static/js/2.fd5371a8.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/iwy/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/iwy/static/js/main.01b01939.chunk.js"
+  },
+  {
+    "revision": "175689c403e3a4a6e349",
+    "url": "/iwy/static/js/runtime~main.ed522ed5.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/iwy/static/media/logo.06e73328.svg"
+  }
+]);
